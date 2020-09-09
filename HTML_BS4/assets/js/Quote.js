ebf0ff66757d0b385f025820054154d1cba16de6
RaphVaudreuil <raphvaudreuil-basket@hotmail.com>
2020-09-08 20:25:04 -0400

    Quote

--- HTML_BS4/assets/js/Quote.js
+++ HTML_BS4/assets/js/Quote.js
@@ -11,13 +11,42 @@ $(document).ready(function () {
 //Estimation of the number of required cages 
 
     //Commercial
-    var estiComme = $('#numberShaftsC').val();
-
+    $(document).keyup(function() {
+        var estiComme = $('#numberShaftC').val();
+        $('#estiComm').val(estiComme);
+    });
+       
     //Residential
-    var apResi = $('#numberApsR').val();
-    var floorsResi = $('#numberFloorsR').val();
-    var apPerFloors = apResi/floorsResi; 
-    var estiResi = apResi/6;
+    $(document).keyup(function() {
+        var apResi = $('#numberApsR').val();
+        var floorsResi = $('#numberFloorsR').val();
+        var aveAps = apResi/floorsResi;
+        var estiResid = apResi/6;
+        $('#estiResi').val(Math.round(estiResid));
+        $('#aveResi').val(Math.round(aveAps));
     
+        if (apResi > 20) {
+            $('#estiResi').val(Math.round(estiResid*2));
+        };
+    
+        if (apResi > 40) {
+            $('#estiResi').val(Math.round(estiResid*3));
+        };
+    
+        if(apResi > 60) {
+            $('#estiResi').val(Math.round(estiResid*4));
+        };
+    });
+
+    //Hybrid or Corporate
+    var occuH = $('#numberOccH').val();
+    var occuC1 = $('#numberOccC1').val();
+    var floorsH = $('#numberFloorsH').val();
+    var floorsC1 = $('#numberFloorsC1').val();
+    var basementsH = $('#numberBasementsH').val();
+    var basementsC1 = $('#numberBasementsC1').val();
+    var totalOccuH = occuH * (floorsH + basementsH);
+    var totlaOccuC1 = occuC1 * (floorsC1 + basementsC1);
+
 
 
